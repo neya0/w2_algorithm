46a10a57c13600519fdabca09ee35874a7f11eb2
neya0 <wltjd1484@gmail.com>
2022-07-15 16:52:01 +0900

    q1, q2 이름 변경 및 q3 추가

--- JS/q2.java
+++ JS/q2.java
@@ -1,19 +1,12 @@
-import java.util.Scanner;
-
 class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int m = sc.nextInt();
-        int n = sc.nextInt();
-        
-        if(m <= 1000 && n <= 1000){
-            for(int a = 0; a < n; a++){
-                for(int b = 0; b < m; b++){
-                     System.out.print("*");
-                }
-                System.out.println();
-            }
+    
+    public String solution(int num) {
+        if(num % 2 == 0){
+            String result = "Even";
+            return result;
+        } else {
+            String result = "Odd";
+            return result;
         }
-
     }
 }
