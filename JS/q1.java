8edab10ead25eec307ca5b66db061e067fd44bee
neya0 <wltjd1484@gmail.com>
2022-07-15 16:20:56 +0900

    알고리즘 문제 2번 - 짝수와 홀수

---
+++ JS/q1.java
@@ -0,0 +1,12 @@
+class Solution {
+    
+    public String solution(int num) {
+        if(num % 2 == 0){
+            String result = "Even";
+            return result;
+        } else {
+            String result = "Odd";
+            return result;
+        }
+    }
+}
